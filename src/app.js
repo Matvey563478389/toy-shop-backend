05749d3969dac5785c3dc60d185b44bf334e5a20
matvey <vvv.chebanov@gmail.com>
2026-03-25 00:51:04 +0300

    feat: revert changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
