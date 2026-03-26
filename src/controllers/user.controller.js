3f72ed11234c58d3bbb5d71451ed957a91f1f037
matvey <vvv.chebanov@gmail.com>
2026-03-27 00:35:10 +0300

    feat: add create user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,31 @@ exports.getUsers = async (req, res) => {
     const result = await pool?.query('SELECT * FROM users')
     res.status(200).json(result.rows)
  } catch (error) {
-   console.log('Error: ', error)
+   console.error('Error: ', error)
  }
+}
+
+exports.createUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+
+    if (!email) res.status(400).json({ message: 'Email is required' });
+    if (!password) res.status(400).json({ message: 'Password is required' });
+    if (!name) res.status(400).json({ message: 'Name is required' });
+
+    const userExist = await pool?.query('SELECT * FROM users WHERE email = $1', [email])
+
+    if (userExist.rows.length > 0) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+
+    const result = await pool?.query(
+      'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+      [name, email, password]
+    )
+
+    res.status(200).json(result.rows[0])
+  } catch (error) {
+    console.error('Error: ', error)
+  }
 }
