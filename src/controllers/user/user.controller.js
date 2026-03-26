9c1cf245537682bf79f56ed9aff500ec06b9c3e1
matvey <vvv.chebanov@gmail.com>
2026-03-26 23:28:08 +0300

    feat: add minimal get users contoller

---
+++ src/controllers/user/user.controller.js
@@ -0,0 +1,10 @@
+const pool = require('../../config/db');
+
+exports.getUsers = async (req, res) => {
+ try {
+    const result = await pool?.query('SELECT * FROM users')
+    res.status(200).json(result.rows)
+ } catch (error) {
+   console.log('Error: ', error)
+ }
+}
