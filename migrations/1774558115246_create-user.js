014f4f6a805ec7160e2a7123c2354524c793cc6f
matvey <vvv.chebanov@gmail.com>
2026-03-27 00:31:14 +0300

    feat: add password filed to migration

--- migrations/1774558115246_create-user.js
+++ migrations/1774558115246_create-user.js
@@ -3,6 +3,7 @@ exports.up = (pgm) => {
     id: 'id',
     name: { type: 'varchar(100)', notNull: true },
     email: { type: 'varchar(255)', notNull: true, unique: true },
+    password: { type: 'varchar(255)', notNull: true },
     created_at: {
       type: 'timestamp',
       default: pgm.func('current_timestamp'),
