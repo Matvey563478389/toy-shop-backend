b147035a3cf9d405ef30134fc76d7a9649692796
matvey <vvv.chebanov@gmail.com>
2026-03-30 23:17:19 +0300

    feat: add get me route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const userRouter = express.Router()
 const userController = require('../controllers/user.controller')
+const {protect} = require("../middleware/auth.middleware");
 
 userRouter.get('/users', userController.getUsers)
 userRouter.post('/user/sign-up', userController.signUp)
 userRouter.post('/user/sign-in', userController.signIn)
+userRouter.get('/user/me', protect, userController.getMe);
 
 module.exports = userRouter;
