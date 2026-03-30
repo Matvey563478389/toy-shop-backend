e23255c7f643025743720ed349a4eeae756754bc
matvey <vvv.chebanov@gmail.com>
2026-03-31 00:23:45 +0300

    feat: add admin check before get all users

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,8 +2,9 @@ const express = require('express')
 const userRouter = express.Router()
 const userController = require('../controllers/user.controller')
 const {protect} = require("../middleware/auth.middleware");
+const { isAdmin } = require('../middleware/admin.middleware');
 
-userRouter.get('/users', userController.getUsers)
+userRouter.get('/users', protect, isAdmin, userController.getUsers)
 userRouter.post('/user/sign-up', userController.signUp)
 userRouter.post('/user/sign-in', userController.signIn)
 userRouter.get('/user/me', protect, userController.getMe);
